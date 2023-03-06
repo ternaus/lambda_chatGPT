920261c3368ea5090e4cc723f7cd48f9f7471e3b
Vladimir Iglovikov <iglovikov@gmail.com>
2023-03-06 15:26:14 -0800

    Added mixpanel tracking

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
+import os
+
 import ujson as json
 from dotenv import load_dotenv
+from mixpanel import Mixpanel
 
 from src.generate_response import get_reply
 
 headers = {"Access-Control-Allow-Origin": "*", "Access-Control-Allow-Methods": "OPTIONS,POST"}
 
 load_dotenv()  # take environment variables from .env.
+
+mp = Mixpanel(os.environ["MIXPANEL_TOKEN"])
 
 
 def lambda_handler(event, context=None):
@@ -20,8 +25,12 @@ def lambda_handler(event, context=None):
     if "action" not in body:
         return {"statusCode": 400, "headers": headers}
 
-    if body["action"] == "reply":
+    action = body["action"]
+
+    if action == "reply":
         response = get_reply(body["text"])
+        distinct_id = body.get("metadata", {}).get("distinct_id", "unknown")
+        mp.track(distinct_id, {"action": "reply", **body.get("metadata", {})})
 
     return {
         "statusCode": 200,
